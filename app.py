982f4a9615c1cc7610bda14eb4c1cd3c9f3caf03
AyushMagdum15 <ayushmagdum15@gmail.com>
2025-11-24 23:17:59 +0530

    app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ import plotly.graph_objects as go
 # -----------------------------------------
 # 1) LOAD DATA
 # -----------------------------------------
-DATA_PATH = r"\subcounty_metrics.csv"
+DATA_PATH = r"subcounty_metrics.csv"
 
 df = pd.read_csv(DATA_PATH)
 
@@ -259,4 +259,5 @@ def update_dashboard(selected_counties, metric, topn):
 # -----------------------------------------
 if __name__ == "__main__":
     app.run(debug=True, port=8050)
+
 
